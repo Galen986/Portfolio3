86822895c1bb1e3ae56fad6785da69121af5173c
FEITEN <guisilva4410@gmail.com>
2025-05-06 20:16:02 -0300

    Atualizar o JavaScript.js

--- styles/JavaScript.js
+++ styles/JavaScript.js
@@ -1,15 +1,11 @@
-// Seleciona o botão e o corpo da página
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-
-// Alterna entre os temas claro e escuro
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-
-    // Altera o texto do botão com base no tema
-    if (body.classList.contains('dark-mode')) {
-        themeToggle.textContent = 'Modo Claro';
-    } else {
-        themeToggle.textContent = 'Modo Escuro';
+// Evento para navegação
+window.addEventListener('popstate', (event) => {
+    if (!event.state) {
+        window.close(); // Sai do site
     }
 });
+
+// Configuração inicial
+window.onload = () => {
+    history.replaceState({}, '', location.href);
+};
