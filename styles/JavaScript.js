08e238fcd6b12c3c746f86f9bd2526a5296f0cfe
FEITEN <guisilva4410@gmail.com>
2025-05-06 16:06:41 -0300

    Atualizar o JavaScript.js

--- styles/JavaScript.js
+++ styles/JavaScript.js
@@ -1,25 +1,7 @@
-// Função para navegar entre as páginas
-function navigateTo(page) {
-    document.querySelectorAll('main').forEach(main => main.style.display = 'none');
-    document.getElementById(page).style.display = 'block';
-
-    if (page !== 'home') {
-        history.pushState({ page }, '', `#${page}`);
-    } else {
-        history.replaceState({ page }, '', `#${page}`);
-    }
-}
-
-// Evento "voltar"
-window.addEventListener('popstate', (event) => {
-    if (event.state && event.state.page) {
-        navigateTo(event.state.page);
-    } else {
-        window.close(); // Sai do site
-    }
-});
-
-// Configuração inicial
 window.onload = () => {
-    navigateTo('home');
+    history.replaceState({}, '', location.href);
+};
+
+window.onpopstate = () => {
+    window.close(); // Fecha o site ao voltar
 };
